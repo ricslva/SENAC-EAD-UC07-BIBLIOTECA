044f839f5ab9aabf6427d788da731eb8f1f8d7da
Ricardo <r.cortez@uol.com.br>
2023-04-13 18:32:43 -0300

    Correção de Telas

--- Models/EmprestimoService.cs
+++ Models/EmprestimoService.cs
@@ -34,7 +34,35 @@ namespace Biblioteca.Models
         {
             using(BibliotecaContext bc = new BibliotecaContext())
             {
-                return bc.Emprestimos.Include(e => e.Livro).ToList();
+
+                IQueryable<Emprestimo> query;
+                
+                if(filtro != null)
+                {
+                    //definindo dinamicamente a filtragem
+                    switch(filtro.TipoFiltro)
+                    {
+                        case "Usuario":
+                            query = bc.Emprestimos.Where(e => e.NomeUsuario.Contains(filtro.Filtro));
+                        break;
+
+                        case "Livro":
+                            query = bc.Emprestimos.Where(e => e.Livro.Titulo.Contains(filtro.Filtro));
+                        break;
+
+                        default:
+                            query = bc.Emprestimos;
+                        break;
+                    }
+                }
+                else
+                {
+                    // caso filtro não tenha sido informado
+                    query = bc.Emprestimos;
+                }
+
+                //ordenação padrão
+                return query.Include(e => e.Livro).ToList();
             }
         }
 
